ac226d4f7b9565dd729cd9aa9b4651b5fc007515
nicjpa <nicjpa@HM53>
2023-09-04 01:38:38 +0200

    Add project files.

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoulmateSeeker.Entities;
+
+namespace SoulmateSeeker.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
